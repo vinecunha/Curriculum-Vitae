664415fa73bc186b1b6ab5bf9b578b65f1ef4795
Vinicius Cunha Martins <vcunha@id.uff.br>
2021-07-30 20:06:44 -0300

    Upload 30/07

--- js/box-toggle.js
+++ js/box-toggle.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
-    $('.tgl').before('<span><h2>&#xf142 Experiência Profissional &#xf0d7</h2></span>');
+    $('.tgl').before('<span><h2>&#xf142 Experiência Profissional</h2>&#xf0d7</span>');
     $('.tgl').css('display','none')
     
-    $('.tgl2').before('<span><h2>&#xf19d Formação &#xf0d7</h2></span>');
+    $('.tgl2').before('<span><h2>&#xf19d Formação</h2>&#xf0d7</span>');
     $('.tgl2').css('display', 'none')
     
-    $('.tgl3').before('<span><h2>&#xf19d Formação Complementar &#xf0d7</h2></span>');
+    $('.tgl3').before('<span><h2>&#xf19d Formação Complementar</h2>&#xf0d7</span>');
     $('.tgl3').css('display', 'none')
     
-    $('.tgl4').before('<span><h2>&#xf142 Competências &#xf0d7</h2></span>');
+    $('.tgl4').before('<span><h2>&#xf142 Competências</h2>&#xf0d7</span>');
     $('.tgl4').css('display', 'none')
     
-    $('.tgl5').before('<span><h2>&#xf129 Informações Complementares &#xf0d7</h2></span>');
+    $('.tgl5').before('<span><h2>&#xf129 Informações Complementares</h2>&#xf0d7</span>');
     $('.tgl5').css('display', 'none')
                 
     $('span', '#box-toggle').click(function() {
